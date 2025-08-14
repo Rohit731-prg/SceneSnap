0af94656f988101816c94849fd451f9a956d13b6
Rohit731-prg <rohitsingha821380@gmail.com>
2025-08-14 17:11:00 +0530

    working in signup page

--- Client/src/components/Login.tsx
+++ Client/src/components/Login.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import useUserStore from "../store/userStore";
 import { useNavigate } from "react-router-dom";
+import { FaUser, FaLock  } from "react-icons/fa";
+import LoginL from '../assets/Login.json';
+import Lottie  from 'lottie-react';
 
 function Login() {
     const navigate = useNavigate();
-    const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUXVHwPtfOqBe9mm099xh1DJ71zWVHuCwN9vGjkx1fi09kDx7_wyaldRkzWWRQJ6gT1U&usqp=CAU";
-    const [showPass, setShowPass] = useState<boolean>(true);
+    const [showPass, setShowPass] = useState<boolean>(false);
 
     const [user, setUser] = useState({
         email: "",
@@ -23,50 +25,54 @@ function Login() {
 
     }
     return (
-        <main className="bg-slate-800 py-10 px-80 font-primary h-screen">
-            <img src={image} alt="" className="w-full h-96 object-cover rounded-lg" />
+        <main className="bg-slate-800 font-primary h-screen flex flex-row items-center justify-center">
 
-            <section className="my-10">
-                <p className="text-center text-white font-semibold text-4xl">Welcome Back</p>
+            <section className="w-1/3 px-10">
+                <p className="text-white font-semibold text-4xl">Welcome Back</p>
+                <p className="text-slate-400 font-primary">Sign in to your account</p>
 
-                <form
-                    onSubmit={handelSubmit}
-                    className="flex flex-col gap-4 items-center justify-center w-full px-96 mt-10">
-                    <input
-                        value={user.email}
-                        onChange={(e) => setUser({ ...user, email: e.target.value })}
-                        placeholder="Enter Email Address"
-                        className="px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg"
-                        type="email" />
-
-                    <div className="flex items-center justify-between px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg">
+                <form className="py-10 pr-20" onSubmit={handelSubmit}>
+                    <div className="flex flex-row items-center bg-slate-500 px-5 rounded-md">
+                        <FaUser />
                         <input
-                            placeholder="Enter Password"
-                            value={user.password}
-                            onChange={(e) => setUser({ ...user, password: e.target.value })}
-                            className=" outline-none border-none w-full"
-                            type={showPass ? "password" : "text"} />
+                            onChange={(e) => setUser({ ...user, email: e.target.value })}
+                            className="w-full border-none outline-none px-3 py-2"
+                            type="email"
+                            placeholder="Email Address" />
 
-                        <div>
-                            <button
-                                type="button"
-                                onClick={() => setShowPass(!showPass)}
-                            >
-                                {showPass ? <FaEye /> : <FaEyeSlash />}
-                            </button>
+                    </div>
+
+                    <div className="flex flex-row items-center bg-slate-500 px-5 justify-between mt-5 rounded-md">
+                        <div className="flex flex-row items-center bg-slate-500">
+                            <FaLock />
+                            <input
+                                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                                className="w-full border-none outline-none px-3 py-2"
+                                type={showPass ? "text" : "password"}
+                                placeholder="Password" />
                         </div>
+
+                        <button
+                        type="button"
+                        onClick={() => setShowPass(!showPass)}
+                        >
+                            {showPass ? <FaEyeSlash /> : <FaEye  />}
+                        </button>
                     </div>
 
                     <button
-                        className="text-white px-5 rounded-lgpx-5 py-2 border-none outline-none bg-blue-500 w-full rounded-lg"
-                    >
-                        Lon in
+                    type="submit"
+                    className="w-full mt-5 py-2 bg-blue-500 rounded-md active:scale-95 transition">
+                        Sign In
                     </button>
                 </form>
 
-                <p className="text-center text-white mt-10">Don't have an account?
-                    <span onClick={() => navigate('/signup')} className="text-blue-500 cursor-pointer">Sign up</span>
+                <p className="text-white mt-5">Don't have an account ?
+                    <span onClick={() => navigate('/signup')} className="text-blue-500 cursor-pointer underline px-2">Sign up</span>
                 </p>
+            </section>
+            <section className="w-1/3">
+                <Lottie  animationData={LoginL} loop={true} />
             </section>
         </main>
     )
--- Client/src/components/SignUp.tsx
+++ Client/src/components/SignUp.tsx
@@ -1,70 +1,129 @@
-import { useState } from "react"
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import React, { useState } from "react";
+import type { ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import Lottie from "lottie-react";
+import sign from '../assets/signup.json';
+
+
+type SignUpUser = {
+    name: string;
+    email: string;
+    phone: string;
+    role: string;
+    password: string;
+    conPassword: string;
+    image: string | null;
+};
 
 const SignUp = () => {
-    const image = 'https://img.freepik.com/premium-photo/dark-room-with-camera-lights_248459-63285.jpg'
-    const [showPass, setShowPass] = useState<Boolean>(true)
-    return (
-        <main className="bg-slate-800 py-10 px-80 font-primary">
-            <img src={image} alt="" className="w-full h-96 object-cover rounded-lg" />
+    const navigate = useNavigate();
+    const [signUpUser, setSignUpUser] = useState<SignUpUser>({
+        name: "",
+        email: "",
+        phone: "",
+        role: "",
+        password: "",
+        conPassword: "",
+        image: null,
+    });
 
-            <section className="my-10">
-                <p className="text-center text-white font-semibold text-4xl">Sign up for ScenesNap</p>
-                <form className="flex flex-col gap-4 items-center justify-center w-full px-96 mt-10">
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            if (typeof reader.result === "string") {
+                setSignUpUser((prev) => ({
+                    ...prev,
+                    image: reader.result as string,
+                }));
+            }
+        };
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <main className="bg-slate-800 font-primary h-screen flex flex-row items-center justify-center">
+            <section className="w-1/3 px-10">
+                <p className="text-white font-semibold text-4xl">Welcome Back</p>
+                <p className="text-slate-400 font-primary">Sign in to your account</p>
+
+                <form action="" className="py-10 pr-20">
                     <input 
-                    placeholder="Full Name"
-                    className="px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg"
+                    value={signUpUser.name}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, name: e.target.value })}
+                    className="py-2 px-5 rounded-md w-full bg-slate-500 outline-none mb-5"
+                    placeholder="Enter Your Full Name..."
                     type="text" />
 
                     <input 
-                    placeholder="Enter Email Address"
-                    className="px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg"
+                    value={signUpUser.email}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, email: e.target.value })}
+                    className="py-2 px-5 rounded-md w-full bg-slate-500 outline-none mb-5"
+                    placeholder="Enter Your Email..."
                     type="email" />
 
                     <input 
-                    placeholder="Enter Phone Number"
-                    className="px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg"
+                    value={signUpUser.phone}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, phone: e.target.value })}
+                    className="py-2 px-5 rounded-md w-full bg-slate-500 outline-none mb-5"
+                    placeholder="Enter Your Phone Number..."
                     type="tel" />
 
-                    <div className="flex items-center justify-between px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg">
-                        <input 
-                        className="w-full border-none outline-none"
-                        placeholder="Enter Password"
-                        type={showPass ? "text" : "password"} />
-                        <button
-                        type="button"
-                        onClick={() => setShowPass(!showPass)}
-                        >
-                            {showPass ? <FaEye /> : <FaEyeSlash />}
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-between px-5 py-2 border-none outline-none bg-slate-500 w-full rounded-lg">
-                        <input 
-                        className="w-full border-none outline-none"
-                        placeholder="Enter Confirm Password"
-                        type={showPass ? "text" : "password"} />
-                        <button
-                        type="button"
-                        onClick={() => setShowPass(!showPass)}
-                        >
-                            {showPass ? <FaEye /> : <FaEyeSlash />}
-                        </button>
-                    </div>
+                    <select 
+                    value={signUpUser.role}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, role: e.target.value })}
+                    className="">
+                        <option value="">Select Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                    </select>
+                    <input 
+                    value={signUpUser.password}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, password: e.target.value })}
+                    className="py-2 px-5 rounded-md w-full bg-slate-500 outline-none mb-5"
+                    placeholder="Enter Your Password..."
+                    type="password" />
+                    <input type="password" />
 
-                    <button
-                    className="px-5 py-2 border-none outline-none bg-blue-500 w-full rounded-lg"
-                    type="submit"
-                    >
-                        Submit Details
-                    </button>
+                    <input 
+                    value={signUpUser.conPassword}
+                    onChange={(e) => setSignUpUser({ ...signUpUser, conPassword: e.target.value })}
+                    className="py-2 px-5 rounded-md w-full bg-slate-500 outline-none mb-5"
+                    placeholder="Confirm Your Password..."
+                    type="password" />
+
+                    <label htmlFor="image">
+                        {signUpUser.image ? (
+                            <div className="w-full h-40">
+                                <img src={signUpUser.image} alt="" className="w-full h-full" />
+                            </div>
+                        ) : (
+                            <div className="w-full h-40 flex flex-col items-center justify-center border-2 border-dashed border-slate-500 rounded-lg bg-gray-600">
+                                <p>Upload Image</p>
+                                <p>Drag and drop, or browse</p>
+                            </div>
+                        )}
+                    </label>
+
+                    <input 
+                    id="image"
+                    className="hidden"
+                    accept="image/*"
+                    onChange={(e) => handleImageChange(e)}
+                    type="file" />
                 </form>
-
-                <p className="text-center mt-5 text-white ">
-                    Already have an account? <a href="#" className="text-blue-500">Login</a>
+                <p className="text-white mt-5">Already have an account ?
+                    <span onClick={() => navigate('/')} className="text-blue-500 cursor-pointer underline px-2">Log in</span>
                 </p>
             </section>
-        </main>
-    )
-}
 
-export default SignUp
+            <section className="w-1/3">
+                <Lottie animationData={sign} />
+            </section>
+        </main>
+    );
+};
+
+export default SignUp;
